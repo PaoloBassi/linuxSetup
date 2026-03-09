c03fafdbbe13dc203e0d7fba64982b73fa5be6e4
Paolo Bassi <paolo.bassi2@flex.com>
2026-03-09 15:42:53 +0100

    feat: remove ulauncher

--- install_scripts/create_soft_links.sh
+++ install_scripts/create_soft_links.sh
@@ -7,11 +7,9 @@ ln -s $SCRIPT_DIR/files/vimrc $HOME/.vimrc || error "Failed to link .vimrc"
 ln -s $SCRIPT_DIR/files/sakura.conf $HOME/.config/sakura/sakura.conf || error "Failed to link sakura config"
 
 # Create directories and extract files
-mkdir -p $HOME/.config/ulauncher || error "Failed to create ulauncher config directory"
 mkdir -p $HOME/.local/share/icons || error "Failed to create icons directory"
 mkdir -p $HOME/.themes || error "Failed to create themes directory"
 
-tar xf $SCRIPT_DIR/files/ulauncher-config.tar.gz -C $HOME/.config/ulauncher/ || error "Failed to extract ulauncher-config.tar.gz file"
 tar xf $SCRIPT_DIR/files/gruvbox-icons.tar.gz -C $HOME/.local/share/icons/ || error "Failed to extract cursor tar.gz file"
 tar xf $SCRIPT_DIR/files/gruvbox-theme.tar.gz -C $HOME/.themes/ || error "Failed to extract theme tar.gz file"
 
