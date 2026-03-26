7663d73d030c4df2be21aa63f60980f090776d1c
Paolo Bassi <paolo.bassi2@flex.com>
2026-03-26 10:01:14 +0100

    fix: include top outer gap in gap toggle to account for Zebar bar

--- windows/files/scripts/toggleGaps.ps1
+++ windows/files/scripts/toggleGaps.ps1
@@ -3,12 +3,14 @@ $glazewmCli = "C:\Program Files\glzr.io\GlazeWM\cli\glazewm.exe"
 
 $content = Get-Content $configPath -Raw
 
+$outerPattern = "(outer_gap:\r?\n\s+top: )'\d+px'(\r?\n\s+right: )'\d+px'(\r?\n\s+bottom: )'\d+px'(\r?\n\s+left: )'\d+px'"
+
 if ($content -match "inner_gap: '0px'") {
     $content = $content -replace "inner_gap: '0px'", "inner_gap: '10px'"
-    $content = $content -replace "(outer_gap:\r?\n\s+top: '\d+px'\r?\n\s+right: )'\d+px'(\r?\n\s+bottom: )'\d+px'(\r?\n\s+left: )'\d+px'", "`${1}'10px'`${2}'10px'`${3}'10px'"
+    $content = $content -replace $outerPattern, "`${1}'50px'`${2}'10px'`${3}'10px'`${4}'10px'"
 } else {
     $content = $content -replace "inner_gap: '\d+px'", "inner_gap: '0px'"
-    $content = $content -replace "(outer_gap:\r?\n\s+top: '\d+px'\r?\n\s+right: )'\d+px'(\r?\n\s+bottom: )'\d+px'(\r?\n\s+left: )'\d+px'", "`${1}'0px'`${2}'0px'`${3}'0px'"
+    $content = $content -replace $outerPattern, "`${1}'40px'`${2}'0px'`${3}'0px'`${4}'0px'"
 }
 
 Set-Content $configPath $content -NoNewline
