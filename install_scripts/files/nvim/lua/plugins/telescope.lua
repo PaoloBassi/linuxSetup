e73cc45a1dcd2d91a9d80a17b1bd502707b40a60
Paolo Bassi <paolo.bassi2@flex.com>
2026-05-20 15:07:25 +0200

    feat: add telescope config (replaces fzf, fzf.vim and ctrlsf)

---
+++ install_scripts/files/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        tag          = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions   = require("telescope.actions")
+
+            telescope.setup({
+                defaults = {
+                    -- mirrors FZF_DEFAULT_OPTS: --height 60% --reverse --border
+                    layout_strategy = "vertical",
+                    layout_config   = { height = 0.6, preview_height = 0.4 },
+                    border          = true,
+                    -- mirror zshrc fzf bindings: ctrl-j/k for navigation, kj to close
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["kj"]    = actions.close,
+                        },
+                        n = {
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                    },
+                    -- focus on result after selection (mirrors ctrlsf_auto_focus)
+                    selection_strategy = "reset",
+                },
+                pickers = {
+                    find_files  = { hidden = true },
+                    live_grep   = {
+                        additional_args = { "--hidden" },
+                        -- mirrors ctrlsf_position = 'right'
+                        layout_config   = { preview_width = 0.5 },
+                    },
+                    buffers     = {
+                        sort_mru          = true,
+                        ignore_current_buffer = true,
+                    },
+                },
+            })
+
+            telescope.load_extension("fzf")
+        end,
+    },
+}
