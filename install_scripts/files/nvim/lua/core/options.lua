95ff951385eb5364c7dc34447c3d8d988e9350c0
Paolo Bassi <paolo.bassi2@flex.com>
2026-05-20 15:05:27 +0200

    feat: add neovim base config with lazy.nvim bootstrap

---
+++ install_scripts/files/nvim/lua/core/options.lua
@@ -0,0 +1,30 @@
+local opt = vim.opt
+
+opt.encoding      = "UTF-8"
+opt.number        = true
+opt.relativenumber = true
+opt.mouse         = "a"
+opt.background    = "dark"
+opt.hlsearch      = true
+opt.expandtab     = true
+opt.tabstop       = 4
+opt.shiftwidth    = 4
+opt.softtabstop   = 4
+opt.showmatch     = true
+opt.incsearch     = true
+opt.autoindent    = true
+opt.smartindent   = true
+opt.hidden        = true
+opt.termguicolors = true
+opt.showmode      = false
+opt.clipboard     = "unnamedplus"
+opt.splitright    = true
+opt.splitbelow    = true
+opt.updatetime    = 300
+opt.undofile      = true
+
+local undodir = vim.fn.expand("$HOME/.config/nvim/.undodir")
+opt.undodir = undodir
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.mkdir(undodir, "p")
+end
