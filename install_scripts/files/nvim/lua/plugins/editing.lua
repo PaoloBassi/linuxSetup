cdc4f7950cf1962dd8471ef1c3d0d93b18640380
Paolo Bassi <paolo.bassi2@flex.com>
2026-05-21 17:23:59 +0200

    feat: add undo-glow with animations for undo/redo/yank/search/comment/cursor

--- install_scripts/files/nvim/lua/plugins/editing.lua
+++ install_scripts/files/nvim/lua/plugins/editing.lua
@@ -22,12 +22,7 @@ return {
         event  = "InsertEnter",
         config = function()
             require("nvim-autopairs").setup({ check_ts = true })
-            -- integrate with nvim-cmp
-            local ok, cmp = pcall(require, "cmp")
-            if ok then
-                local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-                cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-            end
+            -- blink.cmp handles auto_brackets natively
         end,
     },
 
@@ -50,4 +45,105 @@ return {
 
     -- EditorConfig: built-in in nvim 0.9+, plugin only for older versions -----
     { "editorconfig/editorconfig-vim", cond = vim.fn.has("nvim-0.9") == 0 },
+
+    -- Visual glow on undo/redo/yank/paste/search/comment/cursor (new) ----------
+    {
+        "y3owk1n/undo-glow.nvim",
+        event = "VeryLazy",
+        opts = {
+            animation = {
+                enabled      = true,
+                duration     = 300,
+                animation_type = "zoom",
+                window_scoped = true,
+            },
+            -- dark muted variants of Catppuccin Mocha accent colors
+            highlights = {
+                undo    = { hl_color = { bg = "#693232" } },
+                redo    = { hl_color = { bg = "#2F4640" } },
+                yank    = { hl_color = { bg = "#7A683A" } },
+                paste   = { hl_color = { bg = "#325B5B" } },
+                search  = { hl_color = { bg = "#5C475C" } },
+                comment = { hl_color = { bg = "#7A5A3D" } },
+                cursor  = { hl_color = { bg = "#793D54" } },
+            },
+            priority = 2048 * 3,
+        },
+        keys = {
+            { "u",     function() require("undo-glow").undo() end,        mode = "n", noremap = true },
+            { "<C-r>", function() require("undo-glow").redo() end,        mode = "n", noremap = true },
+            { "p",     function() require("undo-glow").paste_below() end, mode = "n", noremap = true },
+            { "P",     function() require("undo-glow").paste_above() end, mode = "n", noremap = true },
+            {
+                "n",
+                function() require("undo-glow").search_next({ animation = { animation_type = "strobe" } }) end,
+                mode = "n", noremap = true,
+            },
+            {
+                "N",
+                function() require("undo-glow").search_prev({ animation = { animation_type = "strobe" } }) end,
+                mode = "n", noremap = true,
+            },
+            {
+                "*",
+                function() require("undo-glow").search_star({ animation = { animation_type = "strobe" } }) end,
+                mode = "n", noremap = true,
+            },
+            {
+                "#",
+                function() require("undo-glow").search_hash({ animation = { animation_type = "strobe" } }) end,
+                mode = "n", noremap = true,
+            },
+            -- comment glow (wraps Comment.nvim's gc/gcc operators)
+            {
+                "gc",
+                function()
+                    local pos = vim.fn.getpos(".")
+                    vim.schedule(function() vim.fn.setpos(".", pos) end)
+                    return require("undo-glow").comment()
+                end,
+                mode = { "n", "x" }, expr = true, noremap = true,
+            },
+            {
+                "gc",
+                function() require("undo-glow").comment_textobject() end,
+                mode = "o", noremap = true,
+            },
+            {
+                "gcc",
+                function() return require("undo-glow").comment_line() end,
+                mode = "n", expr = true, noremap = true,
+            },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("TextYankPost", {
+                callback = function() require("undo-glow").yank() end,
+            })
+            -- beacon-like cursor highlight on significant jumps
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                callback = function()
+                    require("undo-glow").cursor_moved({ animation = { animation_type = "slide" } })
+                end,
+            })
+            -- highlight cursor when re-focusing (tmux pane switch included)
+            vim.api.nvim_create_autocmd("FocusGained", {
+                callback = function()
+                    local utils = require("undo-glow.utils")
+                    local opts  = utils.merge_command_opts("UgCursor", { animation = { animation_type = "slide" } })
+                    local pos   = utils.get_current_cursor_row()
+                    require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
+                        s_row = pos.s_row, s_col = pos.s_col,
+                        e_row = pos.e_row, e_col = pos.e_col,
+                        force_edge = true,
+                    }))
+                end,
+            })
+            -- highlight match when exiting / search cmdline
+            vim.api.nvim_create_autocmd("CmdlineLeave", {
+                callback = function()
+                    require("undo-glow").search_cmd({ animation = { animation_type = "fade" } })
+                end,
+            })
+        end,
+    },
 }
