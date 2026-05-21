03b28bb7c673158eee8667e61e80708a11712dad
Paolo Bassi <paolo.bassi2@flex.com>
2026-05-21 08:51:12 +0200

    feat: change next/previous hunk command from h to c

--- install_scripts/files/nvim/lua/plugins/git.lua
+++ install_scripts/files/nvim/lua/plugins/git.lua
@@ -14,8 +14,8 @@ return {
                 on_attach = function(bufnr)
                     local gs   = package.loaded.gitsigns
                     local opts = { buffer = bufnr }
-                    vim.keymap.set("n", "]h",         gs.next_hunk,  opts)
-                    vim.keymap.set("n", "[h",         gs.prev_hunk,  opts)
+                    vim.keymap.set("n", "]c",         gs.next_hunk,  opts)
+                    vim.keymap.set("n", "[c",         gs.prev_hunk,  opts)
                     vim.keymap.set("n", "<leader>hs", gs.stage_hunk, opts)
                     vim.keymap.set("n", "<leader>hr", gs.reset_hunk, opts)
                     vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
