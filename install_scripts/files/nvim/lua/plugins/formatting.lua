d92effb8bc50641d58bd22afcd2f06bb2d32f7ed
Paolo Bassi <paolo.bassi2@flex.com>
2026-06-26 15:38:15 +0200

    feat: disable format-on-save, keep manual format only

--- install_scripts/files/nvim/lua/plugins/formatting.lua
+++ install_scripts/files/nvim/lua/plugins/formatting.lua
@@ -1,17 +1,13 @@
 return {
     {
         "stevearc/conform.nvim",
-        event = "BufWritePre",
+        event = "VeryLazy",
         config = function()
             require("conform").setup({
                 formatters_by_ft = {
                     c   = { "clang_format" },
                     cpp = { "clang_format" },
                     lua = { "stylua" },
-                },
-                format_on_save = {
-                    timeout_ms = 500,
-                    lsp_format = "fallback",
                 },
             })
             vim.keymap.set({ "n", "v" }, "<leader>cf", function()
